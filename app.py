064377540c21c30456c7a2ba52cce34c20430127
Dylan <dylszcz@udel.edu>
2026-02-20 11:16:51 -0500

    Streamlit Intro

--- app.py
+++ app.py
@@ -1,4 +1,45 @@
 import streamlit as st
-st.title("AI Course Manager")
-st.write("Week 3 UI layer connected to the Week 2 Assignment Manager data model.")
-st.button("Click me")
+
+
+# Step 1: Header First (Text Elements)
+
+st.title("Course Management")
+st.header("Course Management Dashboard")
+st.caption("MISY350")
+st.divider()
+
+# Step 2: Define Assignments List (Data Continuity)
+
+assignments = [
+    {
+        'id':"HW1",
+        'title':'Introduction to Database',
+        'description':'Basics of database design',
+        'points':100,
+        'type':'homework'
+    },
+    {
+        'id':"HW2",
+        'title':'Normalization',
+        'description':'Normalize the table designs',
+        'points':100,
+        'type':'lab'
+    }
+]
+
+# Step 3: Add New Assignment Section (Inputs & Layout)
+
+st.subheader("Add New Assignment")
+with st.container(border = True):
+    col1,col2 = st.columns([2,1])
+
+    with col1:
+        with st.container(border = True):
+            st.markdown('### Assignment Details')
+            title = st.text_input('Assignment Title', placeholder = 'Homework')
+            description = st.text_input("Assignment Description")
+            points = st.number_input('Points')
+    with col2:
+        st.markdown('**Time & Type**')
+        due_date = st.date_input('Due Date')
+        type = st.radio('Type',('Homework','Lab'))
