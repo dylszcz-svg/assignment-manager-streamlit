83330d59646c7af50e1de63196ccfa1fcdc144ad
Dylan <dylszcz@udel.edu>
2026-03-11 11:13:06 -0400

    streamlit session

--- app_day6.py
+++ app_day6.py
@@ -8,6 +8,21 @@ import time
 st.set_page_config(page_title="Course Manager", layout="centered")
 st.title("Course Manager App")
 
+# Initialize Session State
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+
+if "user" not in st.session_state:
+    st.session_state["user"] = None
+
+if "role" not in st.session_state:
+    st.session_state["role"] = None 
+
+if "page" not in st.session_state:
+    st.session_state["page"] = "login" # or 'register'
+
+
+
 # --- DATA LOADING ---
 json_file = Path("users.json")
 users = []
@@ -18,7 +33,7 @@ users = [{
 "full_name": "System Admin",
 "password": "123ssag@43AE",
 "role": "Admin",
-"registered_at": "..."
+"registered_at": str(datetime.now())
 }]
 
 
@@ -27,51 +42,80 @@ if json_file.exists():
         users = json.load(f)
 
 
-# --- LOGIN ---
-st.subheader("Log In")
-with st.container(border=True):
-    email_input = st.text_input("Email", key="login_email")
-    password_input = st.text_input("Password", type="password", key="login_password")
-    
-    if st.button("Log In", type='primary', use_container_width=True):
-        with st.spinner("Logging in..."):
-            time.sleep(2) # Fake backend delay
-            
-            # Find user
-            found_user = None
-            for user in users:
-                if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
-                    found_user = user
-                    break
-            
-            if found_user:
-                st.success(f"Welcome back, {found_user['email']}!")
-                time.sleep(2)
-                st.rerun()
-            else:
-                st.error("Invalid credentials")
+if st.session_state['role'] == 'Admin':
+    st.markdown("This is the Admin UI - Dashboard")
 
-# --- REGISTRATION ---
-st.subheader("New Instructor Account")
-with st.container(border=True):
-    new_email = st.text_input("Email Address", key="reg_email")
-    new_password = st.text_input("Password", type="password", key="reg_password")
-    
-    if st.button("Create Account", type = 'secondary', use_container_width=True):
-        with st.spinner("Creating account..."):
-            time.sleep(2) # Fake backend delay
-            # ... (Assume validation logic here) ...
-            users.append({
-                "id": str(uuid.uuid4()),
-                "email": new_email,
-                "password": new_password,
-                "role": "Instructor"
-            })
-            with open(json_file, "w") as f:
-                json.dump(users, f, indent=4)
-            st.success(f"Account created: {new_email}")
+    if st.button("Log Out"):
+        with st.spinner("logging out..."):
             time.sleep(4)
+            st.session_state["logged_in"] = False
+            st.session_state["user"] = None
+            st.session_state["role"] = None
             st.rerun()
 
-st.write("---")
-st.dataframe(users)
+elif st.session_state['role'] == 'Instructor':
+    st.markdown("This is the Instructor Dashboard")
+
+else:
+
+
+
+    # --- LOGIN ---
+    st.subheader("Log In")
+    with st.container(border=True):
+        email_input = st.text_input("Email", key="login_email")
+        password_input = st.text_input("Password", type="password", key="login_password")
+        
+        if st.button("Log In", type='primary', use_container_width=True):
+            with st.spinner("Logging in..."):
+                time.sleep(2) # Fake backend delay
+                
+                # Find user
+                found_user = None
+                for user in users:
+                    if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
+                        found_user = user
+                        break
+                
+                if found_user:
+                    st.success(f"Welcome back, {found_user['email']}!")
+                    st.session_state["logged_in"] = True
+                    st.session_state["user"] = found_user
+                    st.session_state["role"] = found_user["role"]
+                    time.sleep(2)
+                    st.rerun()
+                else:
+                    st.error("Invalid credentials")
+
+    # --- REGISTRATION ---
+    st.subheader("New Instructor Account")
+    with st.container(border=True):
+        new_email = st.text_input("Email Address", key="reg_email")
+        new_password = st.text_input("Password", type="password", key="reg_password")
+        
+        if st.button("Create Account", type = 'secondary', use_container_width=True):
+            with st.spinner("Creating account..."):
+                time.sleep(2) # Fake backend delay
+                # ... (Assume validation logic here) ...
+                users.append({
+                    "id": str(uuid.uuid4()),
+                    "email": new_email,
+                    "password": new_password,
+                    "role": "Instructor"
+                })
+                with open(json_file, "w") as f:
+                    json.dump(users, f, indent=4)
+                st.success(f"Account created: {new_email}")
+                time.sleep(4)
+                st.rerun()
+
+    st.write("---")
+    st.dataframe(users)
+
+
+#with st.sidebar:
+ #   if "logged_in" in st.session_state and st.session_state["logged_in"] != None:
+  #      user = st.session_state["user"]
+   #     st.markdown(f"Welcome {user['email']}!")
+    #else:
+     #   st.markdown("Welcome!")
